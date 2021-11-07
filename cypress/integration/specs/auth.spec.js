0a8db3ce34847d31b4f78c7b7c14bb12406d3009
rajitha.mail48 <rajitha.mail48@gmail.com>
2021-11-07 12:21:09 +0100

    updated test script

--- cypress/integration/specs/auth.spec.js
+++ cypress/integration/specs/auth.spec.js
@@ -1,41 +1,41 @@
 
-it('/Register with body ',()=>{
+describe('/user/register',()=>{
+  it('/Register with body ',()=>{
 
-  let body = {
-
-    regno: 'AB004',
-    age: 28,
-    name: {
-      fname: 'John',
-      lname: 'Doe'
+    let body = {
+  
+      regno: 'AB004',
+      age: 28,
+      name: {
+        fname: 'John',
+        lname: 'Doe'
+      }
+   
     }
- 
-  }
-
-  cy.request({
-    method: 'POST',
-    url: 'http://localhost:3000/api/user/register',
-    body: body,
-    failOnStatusCode: false
-  }).then((response)=>{
-    expect(response.status).to.eq(200);
-    expect(response.body.regno).to.eq('AB004');
-    expect(response.body.age).to.eq(28);
-
-  });
-
- 
-}); 
-
-it('/Register without body',()=>{
   
     cy.request({
       method: 'POST',
       url: 'http://localhost:3000/api/user/register',
+      body: body,
       failOnStatusCode: false
     }).then((response)=>{
-      expect(response.status).to.eq(400);
-    });
+      expect(response.status).to.eq(200);
+      expect(response.body.regno).to.eq('AB004');
+      expect(response.body.age).to.eq(28);
   
-   
+    });
+
   }); 
+})
+
+// it('/Register without body',()=>{
+  
+//     cy.request({
+//       method: 'POST',
+//       url: 'http://localhost:3000/api/user/register',
+//       failOnStatusCode: false
+//     }).then((response)=>{
+//       expect(response.status).to.eq(400);
+//     });
+
+//   }); 
